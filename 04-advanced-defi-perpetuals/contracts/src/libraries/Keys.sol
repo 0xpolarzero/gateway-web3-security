772f1f4630d25a65f22f56d3803a08dbc6d59ce3
polarzero <0xpolarzero@gmail.com>
2023-10-26 20:29:21 +0200

    Add constants

--- 04-advanced-defi-perpetuals/contracts/src/libraries/Keys.sol
+++ 04-advanced-defi-perpetuals/contracts/src/libraries/Keys.sol
@@ -9,8 +9,30 @@ library Keys {
     /* -------------------------------------------------------------------------- */
     /*                                  PROTOCOL                                  */
     /* -------------------------------------------------------------------------- */
+
+    /// @dev The precision of the collateral token
+    uint256 internal constant COLLATERAL_TOKEN_PRECISION = 1e6;
+    /// @dev The decimals of the collateral token
+    uint8 internal constant COLLATERAL_TOKEN_DECIMALS = 6;
+    /// @dev The precision of the index token
+    uint256 internal constant INDEX_TOKEN_PRECISION = 1e8;
+
+    /// @dev The additional precision for internal calculations (1e12)
+    uint256 internal constant ADDITIONAL_COLLATERAL_PRECISION = 1e18 / COLLATERAL_TOKEN_PRECISION;
+    /// @dev The additional precision for internal calculations (1e10)
+    uint256 internal constant ADDITIONAL_INDEX_PRECISION = 1e18 / INDEX_TOKEN_PRECISION;
+
     /// @dev The maximum percentage of total liquidity that can be actively used
     uint256 internal constant MAX_EXPOSURE = 70; // 70%
+
+    /// @dev The maximum leverage allowed for a position before liquidation
+    uint256 internal constant MAX_LEVERAGE = 15 * COLLATERAL_TOKEN_PRECISION; // 15x
+
+    /// @dev The minimum size allowed for a position
+    uint256 internal constant MIN_POSITION_SIZE = 5 * COLLATERAL_TOKEN_PRECISION; // 5 USD
+
+    /// @dev The minimum amount of collateral required to open a position
+    uint256 internal constant MIN_POSITION_COLLATERAL = 10 * COLLATERAL_TOKEN_PRECISION; // 10 USD
 
     /* -------------------------------------------------------------------------- */
     /*                                   ORACLE                                   */
@@ -18,6 +40,9 @@ library Keys {
 
     /// @dev The maximum time allowed for a price update
     uint256 internal constant MAX_ORACLE_RESPONSE_TIMEOUT = 3 hours;
+
+    /// @dev The precision of the price returned by the oracle
+    uint256 internal constant PRICE_FEED_PRECISION = 1e8; // 8 decimals
 
     /* -------------------------------------------------------------------------- */
     /*                                ERC4626 VAULT                               */
