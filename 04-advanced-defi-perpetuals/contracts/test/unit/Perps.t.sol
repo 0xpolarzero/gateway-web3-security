aa44dc0b25fbf4e374ec123614ef2f0666b417ae
polarzero <0xpolarzero@gmail.com>
2023-10-26 13:31:38 +0200

    Complete withdrawLiquidity unit tests

--- 04-advanced-defi-perpetuals/contracts/test/unit/Perps.t.sol
+++ 04-advanced-defi-perpetuals/contracts/test/unit/Perps.t.sol
@@ -28,6 +28,8 @@ contract PerpsTest is Test {
     error OracleLib__StalePrice();
     // SafeTransferLib
     error TransferFromFailed();
+    // ERC4626
+    error WithdrawMoreThanMax();
 
     /* -------------------------------------------------------------------------- */
     /*                                   EVENTS                                   */
@@ -35,12 +37,21 @@ contract PerpsTest is Test {
 
     // ERC4626
     event Deposit(address indexed by, address indexed owner, uint256 assets, uint256 shares);
+    event Withdraw(address indexed by, address indexed to, address indexed owner, uint256 assets, uint256 shares);
 
     /* -------------------------------------------------------------------------- */
-    /*                                  CONSTANTS                                 */
+    /*                                   STORAGE                                  */
     /* -------------------------------------------------------------------------- */
+    /* -------------------------------- CONSTANTS ------------------------------- */
 
     uint256 private constant DEPOSIT_AMOUNT = 1_000e6; // 1,000 USDC
+
+    address private constant ALICE = address(1);
+    address private constant BOB = address(2);
+
+    /* -------------------------------- VARIABLES ------------------------------- */
+
+    uint256 withdrawnLiquidityAmount;
 
     /* -------------------------------------------------------------------------- */
     /*                                    SETUP                                   */
@@ -120,7 +131,78 @@ contract PerpsTest is Test {
     }
 
     /* -------------------------------------------------------------------------- */
-    /*                                   ORACLE                                   */
+    /*                              withdrawLiquidity                             */
+    /* -------------------------------------------------------------------------- */
+
+    modifier withdrawnLiquidity() {
+        // Calculate available liquidity
+        uint256 availableLiquidity = perps.getAvailableLiquidity();
+        withdrawnLiquidityAmount = availableLiquidity / 2;
+
+        // Withdraw liquidity
+        perps.withdrawLiquidity(withdrawnLiquidityAmount);
+        _;
+    }
+
+    /// @dev Transfer the collateral back to the provider
+    function test_withdrawLiquidity_transfersCollateralBackToProvider()
+        external
+        depositedLiquidity
+        withdrawnLiquidity
+    {
+        // Check the balance of the provider
+        uint256 balance = IERC20(address(collateralAsset.token)).balanceOf(address(this));
+        assert(balance == withdrawnLiquidityAmount);
+    }
+
+    /// @dev Update the total liquidity
+    function test_withdrawLiquidity_updatesTotalLiquidity() external depositedLiquidity withdrawnLiquidity {
+        // Check the total liquidity
+        uint256 totalLiquidity = perps.totalLiquidity();
+        assert(totalLiquidity == DEPOSIT_AMOUNT - withdrawnLiquidityAmount);
+    }
+
+    /// @dev Emit the event with correct parameters
+    function test_withdrawLiquidity_emitsEvent() external depositedLiquidity withdrawnLiquidity {
+        // Shares are the same as the amount withdrawn (since there is only one provider) * decimals offset
+        uint256 expectedShares = withdrawnLiquidityAmount * 10 ** Keys.DECIMALS_OFFSET;
+
+        // Check the event
+        vm.expectEmit();
+        // by, to, owner, assets, shares
+        // by, to & owner are the same address
+        emit Withdraw(address(this), address(this), address(this), withdrawnLiquidityAmount, expectedShares);
+        perps.withdrawLiquidity(withdrawnLiquidityAmount);
+    }
+
+    /// @dev Revert if the amount is zero
+    function test_withdrawLiquidity_revertsIfAmountZero() external depositedLiquidity {
+        vm.expectRevert(Perps.Perps_ZeroValueNotAllowed.selector);
+        perps.withdrawLiquidity(0);
+    }
+
+    /// @dev Revert when trying to withdraw more than available for a provider
+    function test_withdrawLiquidity_revertsIfMoreThanShares() external depositedLiquidity {
+        // Deposit more liquidity as another provider
+        _depositLiquidityAs(ALICE, DEPOSIT_AMOUNT * 10);
+
+        vm.expectRevert(WithdrawMoreThanMax.selector);
+        perps.withdrawLiquidity(DEPOSIT_AMOUNT + 1);
+    }
+
+    /// @dev Revert if there is not enough available liquidity
+    function test_withdrawLiquidity_revertsIfNotEnoughAvailableLiquidity() external depositedLiquidity {
+        // Calculate the max amount of liquidity that can be withdrawn
+        // Basically: (total liquidity * max exposure percentage) - totalPnL - currently used
+        // currently used = total open interest
+        uint256 maxAmount = perps.getAvailableLiquidity();
+
+        vm.expectRevert(abi.encodeWithSelector(Perps.Perps_NotEnoughLiquidity.selector, maxAmount));
+        perps.withdrawLiquidity(maxAmount + 1);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                                  OracleLib                                 */
     /* -------------------------------------------------------------------------- */
 
     /// @dev Return the correct price from the oracle
@@ -139,5 +221,17 @@ contract PerpsTest is Test {
         vm.warp(block.timestamp + Keys.MAX_ORACLE_RESPONSE_TIMEOUT + 1);
         vm.expectRevert(OracleLib__StalePrice.selector);
         perps.getCollateralPrice();
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                              HELPER FUNCTIONS                              */
+    /* -------------------------------------------------------------------------- */
+
+    function _depositLiquidityAs(address provider, uint256 amount) private {
+        MockERC20(address(collateralAsset.token)).mint(provider, amount);
+        vm.startPrank(provider);
+        IERC20(address(collateralAsset.token)).approve(address(perps), amount);
+        perps.depositLiquidity(amount);
+        vm.stopPrank();
     }
 }
